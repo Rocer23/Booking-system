f16069984c1ef2b983b45f4690b1cc297bf5359b
Rocer23 <ip314338@gmail.com>
2025-01-08 15:31:03 +0200

    Added Classes Booking and Facility to DataBase

--- System/admin.py
+++ System/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from System.models import Booking, Room, Facility
 
 # Register your models here.
+admin.site.register(Booking)
+admin.site.register(Room)
+admin.site.register(Facility)
--- System/models.py
+++ System/models.py
@@ -19,4 +19,30 @@ class Room(models.Model):
 
 
 class Booking(models.Model):
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bookings")
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="bookings")
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    creation_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.room}"
+
+    class Meta:
+        verbose_name = "Booking"
+        verbose_name_plural = "Bookings"
+        ordering = ["start_time"]
+
+
+class Facility(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    rooms = models.ManyToManyField(Room, related_name="facilities")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Facility"
+        verbose_name_plural = "Facilities"
+        ordering = ["name"]
