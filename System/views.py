6eec00f30cd3c4c91bc46349850f2fe454198ff7
Rocer23 <ip314338@gmail.com>
2025-01-11 18:24:17 +0200

    Created Views and Urls and Tested DataBase

--- System/views.py
+++ System/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from System.models import Room, Booking
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+def index(request):
+    context = {
+        "render_string": "Hello, World!"
+    }
+    return render(request, template_name='booking/index.html', context=context)
+
+
+def room_list(request):
+    rooms = Room.objects.all()
+    context = {
+        "rooms": rooms,
+    }
+    return render(request, template_name='booking/rooms_list.html', context=context)
+
+
+def book_room(request):
+    if request.method == "POST":
+        room_number = request.POST.get("room-number")
+        start_time = request.POST.get("start-time")
+        end_time = request.POST.get("end-time")
+
+        try:
+            room = Room.objects.get(number=room_number)
+        except ValueError:
+            return HttpResponse(
+                "Wrong room number!",
+                status=400
+            )
+        except Room.DoesNotExist:
+            return HttpResponse(
+                "This room number doesn't exists",
+                status=404
+            )
+        booking = Booking.objects.create(
+            user=request.user,
+            room=room,
+            start_time=start_time,
+            end_time=end_time
+        )
+        return redirect("booking-details", pk=booking.id)
+    else:
+        return render(request, template_name="booking/booking_form.html")
+
+
+def booking_details(request, pk):
+    try:
+        booking = Booking.objects.get(id=pk)
+        context = {
+            "booking": booking
+        }
+        return render(request, template_name="booking/booking_details.html", context=context)
+    except Booking.DoesNotExist:
+        return HttpResponse(
+            "This booking doesn't exists",
+            status=404
+        )
